0a620b6205eb99d67aee23abe24ad5a6989dd48c
danila <spook9400@mail.ru>
2024-01-16 17:58:06 +0300

    add homework 22.2

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import secrets
 import string
+from random import random
 
 from django.conf import settings
 from django.contrib.auth import login
@@ -9,11 +10,11 @@ from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 from django.urls import reverse_lazy, reverse
+from django.views import View
 from django.views.generic import CreateView, UpdateView, DetailView
 
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
-
 
 # Create your views here.
 from django.contrib.auth.forms import UserChangeForm
@@ -29,6 +30,43 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    class RegisterView(CreateView):
+        model = User
+        form_class = UserRegisterForm
+        success_url = reverse_lazy('users:login')
+        template_name = 'users/register.html'
+
+        def form_valid(self, form):
+            response = super().form_valid(form)
+            new_user = form.save()
+            # Создаем и сохраняем токен подтверждения
+            token = ''.join(random.choices(string.ascii_letters + string.digits, k=50))
+            new_user.email_verification_token = token
+            new_user.save()
+            # Отправляем письмо с подтверждением
+            current_site = get_current_site(self.request)
+            mail_subject = 'Подтвердите ваш аккаунт'
+            message = (
+                f'Поздравляем, Вы зарегистрировались на нашем портале!\n'
+                f'Для завершения регистрации и подтверждения вашей электронной почты, '
+                f'пожалуйста, кликните по следующей ссылке:\n'
+                f'http://{current_site.domain}{reverse("users:verify_email", kwargs={"uid": new_user.pk, "token": token})}'
+            )
+            send_mail(subject=mail_subject, message=message, from_email=settings.EMAIL_HOST_USER,
+                      recipient_list=[new_user.email])
+            return response
+
+
+class VerifyEmailView(View):
+    def get(self, request, uid, token):
+        try:
+            user = User.objects.get(pk=uid, email_verification_token=token)
+            user.is_active = True
+            user.save()
+            return render(request, 'users/registration_success.html')  # Покажем сообщение о регистрации
+        except User.DoesNotExist:
+            return render(request, 'users/registration_failed.html')  # Покажем сообщение об ошибке
 
 
 class ProfileView(UpdateView):
@@ -116,7 +154,7 @@ def confirm_user_email(request, user):
     )
 
 
-def generate_password(request):
+def generate_new_password(request):
     new_password = create_secret_key(12)
     request.user.set_password(new_password)
     request.user.save()
